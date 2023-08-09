41f7bc296227820e6f0c3f112ef086e909b84847
doceazedo <me@doceazedo.com>
2023-08-08 22:04:36 -0300

    Ignore HTTPS

--- src/routes/[...url]/+server.ts
+++ src/routes/[...url]/+server.ts
@@ -1,11 +1,12 @@
 import { error, redirect } from '@sveltejs/kit';
 
 export const GET = ({ params }) => {
-	const { url } = params;
-	if (!url.startsWith('https://')) throw error(400, 'URL must start with https://');
+	let { url } = params;
 	if (!url.includes('github.io/')) throw error(400, 'Invalid github.io URL');
 
-	const user = url?.split('https://')?.[1]?.split('.github.io')?.[0];
+	if (url.startsWith('https://')) url = url.split('https://')[0];
+
+	const user = url?.split('.github.io')?.[0];
 	const repo = url?.split('github.io/')[1]?.split('/')?.[0];
 
 	if (!user || !repo) throw error(400, 'Invalid URL');
